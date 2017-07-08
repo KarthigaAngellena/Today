333b6b54d35a44735bf4bcbd35a817a39922c72d
KarthigaAngellena <karthikakeerthi25@gmail.com>
2017-07-08 14:45:47 +0530

    Update Power.java

--- .gitignore/Power.java
+++ .gitignore/Power.java
@@ -6,6 +6,17 @@ public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
 int a,b;
+long c=1;
 System.out.println("Enter the Base");
 a=sc.nextInt();
-S
+System.ouit.println("Enter the Power");
+b=sc.nextInt();
+while(b!=0)
+{
+  c*=a;
+  --b;
+}
+  System.out.println("Result is "+c);
+}
+}
+
