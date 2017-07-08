c3a07cac53d6d6dff4c11fb2715c0e7dd4b97dfb
KarthigaAngellena <karthikakeerthi25@gmail.com>
2017-07-08 12:43:02 +0530

    Create Power.java

---
+++ .gitignore/Power.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class Power
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a,b;
+System.out.println("Enter the Base");
+a=sc.nextInt();
+S
