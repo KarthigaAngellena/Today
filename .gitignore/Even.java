a0508844fc461bfb2235d0756579560f10f1d58b
KarthigaAngellena <karthikakeerthi25@gmail.com>
2017-07-10 11:29:19 +0530

    Create Even.java

---
+++ .gitignore/Even.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class Even 
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a,b;
+a=sc.nextInt();
+b=sc.nextInt();
+for(int i=a;i<b;i++)
+{
+if((i%2)==0)
+{
+System.out.println(i);
+}
+}
+}
+}
