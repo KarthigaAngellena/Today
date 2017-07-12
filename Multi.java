f7c76490a3a50ce88091bd9a5276b4563a236af0
KarthigaAngellena <karthikakeerthi25@gmail.com>
2017-07-12 14:02:33 +0530

    Create Multi.java

---
+++ Multi.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class Multi
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a;
+a=sc.nextInt();
+for(int i=1;i<=16;i++)
+{
+System.out.println(a+"*"+i+"="+(a*i));
+}
+}
+}
